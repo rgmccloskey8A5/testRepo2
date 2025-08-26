28440052ff48b368909e4df4b763074488216de5
colbieW <colbie.west26@paceacademy.org>
2025-08-26 10:12:11 -0400

    Update TaylorSwiftGame.java

--- TaylorSwiftGame.java
+++ TaylorSwiftGame.java
@@ -8,6 +8,41 @@ public class TaylorSwiftGame
 		 * Nilaya is: reputation
 		 * Rebecca is: 1989
 		 */
-		System.out.println("ABC"); 
+
+       private static void speakNow()
+       {
+         System.out.println("You chose Speak Now.");
+	      if(((int) (Math.random() * 3) + 1) == 1)
+	      {
+		      System.out.println("Your lyrics are: \n Drop everything now");
+		      if(scan.nextLine().equals("Sparks Fly"))
+		      {
+			      System.out.println("Congrats! You win!"); 
+		      }
+
+		      else
+		      {
+			      System.out.println("Drop everything now, meet");
+			      if(scan.nextLine().equals("Sparks Fly"))
+			      {
+				      System.out.println("Congrats! You win!"); 
+			      }
+			      else
+			      {
+				      System.out.println("Drop everything now, meet me");
+				      if(s.nextLine().equals("Sparks Fly"))
+				      {
+					      System.out.println("Congrats! You win!"); 
+				      }
+				      else
+				      {
+					      System.out.println("You lose :(");
+				      }
+			      }
+		      }
+	      }
+
+       }
+		//System.out.println("ABC"); 
 	}
 }
