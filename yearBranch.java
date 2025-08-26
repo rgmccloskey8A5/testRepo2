ebfcee48a2ac351b3fd319b6b213972ae8d127a3
rgmccloskey8A5 <rgmccloskey8@gmail.com>
2025-08-26 10:18:54 -0400

    Create yearBranch.java

---
+++ yearBranch.java
@@ -0,0 +1,14 @@
+// import java.io.*;
+// import java.util.*;
+
+// public class yearBranch
+
+// {
+//     System.out.println("You chose 1989. Here are your song lyrics: ");
+
+//     int genNum = (1 + (int)(Math.random()* ((3-1) + 1)));
+//     if(genNum == 1)
+//     {
+//         System.out.println();
+//     }
+// }
