2d7cbff1fa1540a10afdb436df888748994a49e3
colbieW <colbie.west26@paceacademy.org>
2025-09-10 11:27:08 -0400

    dependency injection

--- GameService.java
+++ GameService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class GameService
 {
-    public String getInitialLyrics (string albumName)
+    public String getInitialLyrics (String albumName)
     {
         return "Lyrics for " + albumName; 
     }
