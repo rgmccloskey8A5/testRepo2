9f921faa7e491ca88b798c521b0dd73b5527fd8b
jsmi1462 <126503595+jsmi1462@users.noreply.github.com>
2025-09-10 11:44:41 -0400

    maybe now

---
+++ src/java/com/example/taylorswiftgame/TaylorSwiftGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.taylorswiftgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaylorSwiftGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaylorSwiftGameApplication.class, args);
+    }
+
+}
